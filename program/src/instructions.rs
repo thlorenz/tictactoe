517571a35fcb283241a3b9351739ec305841396d
Thorsten Lorenz <thlorenz@gmx.de>
2022-10-18 17:00:03 -0600

    rs: adding shank annotations to initialize game instruction

--- program/src/instructions.rs
+++ program/src/instructions.rs
@@ -1,10 +1,15 @@
 use borsh::BorshDeserialize;
+use shank::ShankInstruction;
 
 // -----------------
 // Instruction
 // -----------------
-#[derive(BorshDeserialize)]
+#[derive(BorshDeserialize, ShankInstruction)]
+#[rustfmt::skip]
 pub enum TictactoeInstruction {
+    #[account(name = "player_x", signer, desc = "The player initializing the game")]
+    #[account(name = "game_pda", mut, desc="The game PDA")]
+    #[account(name = "system_program", desc="The system program")]
     InitializeGame,
     PlayerJoin,
     PlayerMove(PlayerMove),
