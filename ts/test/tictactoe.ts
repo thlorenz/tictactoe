67b09b1a4e6e58793bc0a6cd1b1b110d8135b650
Thorsten Lorenz <thlorenz@gmx.de>
2022-10-19 16:19:42 -0600

    tests: adding comments to make code review more useful

--- ts/test/tictactoe.ts
+++ ts/test/tictactoe.ts
@@ -8,11 +8,14 @@ import { amman, killStuckProcess } from './utils'
 killStuckProcess()
 
 async function setupPlayerX() {
+  // 1. Create a connection to the local test validator
   const connection = new Connection(LOCALHOST, 'confirmed')
 
+  // 2. Generate a keypair for the first player and have amman label it for us
   const [playerX, playerXPair] = await amman.addr.genLabeledKeypair('player x')
   await amman.airdrop(connection, playerX, 2)
 
+  // 3. Create a transaction handler that will use the first player as signer
   const transactionHandler = amman.payerTransactionHandler(
     connection,
     playerXPair
@@ -27,10 +30,13 @@ async function setupPlayerX() {
 }
 
 test('tx: init game and add player x', async (t) => {
+  // 1. Setup the player and its transaction handler
   const { playerXHandler, playerX, playerXPair } = await setupPlayerX()
 
+  // 2. Generate public key for our game and derive the game PDA
   const [game] = amman.addr.genKeypair()
   const gamePda = await pdaForGame(game)
+  // 3. Label the game PDA so we can identify it in the Amman Explorer
   await amman.addr.addLabels({ gamePda })
 
   const ix = createInitializeGameInstruction({
